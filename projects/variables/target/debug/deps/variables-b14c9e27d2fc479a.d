ed6e96a634d51a38adb94a42641e7dcd80e6d092
rishsinghal <zzxy334@gmail.com>
2021-02-12 13:16:16 +0530

    added

---
+++ projects/variables/target/debug/deps/variables-b14c9e27d2fc479a.d
@@ -0,0 +1,5 @@
+/home/illuminati/DEV/rust-learning/projects/variables/target/debug/deps/variables-b14c9e27d2fc479a: src/main.rs
+
+/home/illuminati/DEV/rust-learning/projects/variables/target/debug/deps/variables-b14c9e27d2fc479a.d: src/main.rs
+
+src/main.rs:
---
+++ projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/illuminati/DEV/rust-learning/projects/variables/target/debug/variables: /home/illuminati/DEV/rust-learning/projects/variables/src/main.rs
